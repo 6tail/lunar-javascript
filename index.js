b7f5c53b3ea02ad2fcbaa1d2b4e5c498ebbc7150
6tail <6tail@6tail.cn>
2021-01-16 20:29:43 +0800

    v1.0.36 更新农历纪念日；新增六曜、物候。

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const {Solar, Lunar, SolarWeek, SolarMonth, SolarSeason, SolarHalfYear, SolarUtil, LunarUtil, HolidayUtil} = require('./lunar.js')
+const {Solar, Lunar, NineStar, EightChar, SolarWeek, SolarMonth, SolarSeason, SolarHalfYear, SolarYear, SolarUtil, LunarUtil, HolidayUtil} = require('./lunar.js')
 
 module.exports = {
   Solar: Solar,
   Lunar: Lunar,
+  NineStar: NineStar,
+  EightChar: EightChar,
   SolarWeek: SolarWeek,
   SolarMonth: SolarMonth,
   SolarSeason: SolarSeason,
   SolarHalfYear: SolarHalfYear,
+  SolarYear: SolarYear,
   SolarUtil: SolarUtil,
   LunarUtil: LunarUtil,
   HolidayUtil: HolidayUtil
